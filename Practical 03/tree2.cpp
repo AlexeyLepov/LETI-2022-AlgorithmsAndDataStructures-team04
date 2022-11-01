0fa96ad698d625cf6d23b237c1b796fa1ca8a772
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-11-01 14:16:13 -0300

    Update tree2.cpp

--- Practical 03/tree2.cpp
+++ Practical 03/tree2.cpp
@@ -29,7 +29,7 @@ class Tree{
         Node *root;
         char **SCREEN;
         int offset;
-        
+
     public:
         Tree ();
         void set_right(); //установка правых
@@ -57,7 +57,7 @@ int Tree::find_tz(){
         if(N1[i].lft) pom[N1[i].lft->d] = 1;//пометки соответсвенных вершин существования
         if(N1[i].rgt) pom[N1[i].rgt->d] = 1;
         if(N1[i].mdl) pom[N1[i].mdl->d] = 1;
-        
+
     }
     for (int i=0; i<nv; i++) tz+=pom[i];//подсчёт верных вершин
     return tz;
@@ -108,7 +108,7 @@ void Tree::OutTree(){
     }
     cout << '\n';
 }
- 
+
 
 void Tree::set_left(){
     ifstream file2;
@@ -145,23 +145,22 @@ void Tree::set_middle(){
 }
 
 Tree::Tree(){//главный конструктор дерева
-    ifstream file1;
     int k;
-    file1.open("tags.txt");
-    file1 >> nv;//установка количества вершин
-    N1 = new Node [nv];//памято по вершины
+
     for (int i=0; i<nv;i++){
-        file1 >> k;
+        std::cin << k;
         N1[i].set_node(k);//создание вершины
-        N1[i].lft=NULL;
-        N1[i].rgt=NULL;
-        N1[i].mdl=NULL;
+        N1[i].lft=NULL; // создание пустых дочерних узлов
+        N1[i].rgt=NULL; // создание пустых дочерних узлов
+        N1[i].mdl=NULL; // создание пустых дочерних узлов
+
+        T1.OutTree();//печать дерева
     }
-    this->set_left();//установка массива левых индексов
+    this->set_left(); //установка массива левых индексов
     this->set_right();
     this->set_middle();
-    file1.close();
-    for (int i=0; i<nv; i++){//склейка вершин в дерево
+
+    for (int i=0; i<nv; i++){ //склейка вершин в дерево
         if (this->left[i]!=0) N1[i].lft = &N1[this->left[i]];
         if (this->right[i]!=0) N1[i].rgt = &N1[this->right[i]];
         if (this->middle[i]!=0) N1[i].mdl = &N1[this->middle[i]];
@@ -172,8 +171,8 @@ Tree::Tree(){//главный конструктор дерева
 }
 
 int main(){
-    Tree T1;//создание дерева
-    T1.OutTree();//печать дерева
+    Tree T1; //создание дерева
+
     cout << endl;
     cout << "bypass in breadth: ";
     T1.BFS();//поиск в ширину с соответствующим выводом
