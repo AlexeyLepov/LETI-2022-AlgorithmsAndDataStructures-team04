afbd743ea04c34b31e78838588dad59cdd4ce2d1
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-11-25 09:31:13 +0300

    graph visualization added

--- Practical 04/GraphVisualization/graph.py
+++ Practical 04/GraphVisualization/graph.py
@@ -5,14 +5,42 @@ import networkx as nx
 # create a directed multi-graph
 G = nx.MultiDiGraph()
 G.add_edges_from([
-    (1, 2),
-    (2, 3),
-    (3, 2),
-    (2, 1),
+    (0, 1),
+    (1, 5),
+    (2, 6),
+    (3, 7),
+    (4, 5),
+    (4, 8),
+    (5, 4),
+    (5, 6),
+    (6, 2),
+    (6, 5),
+    (6, 7),
+    (6, 10),
+    (7, 3),
+    (7, 6),
+    (7, 11),
+    (8, 4),
+    (8, 9),
+    (9, 10),
+    (9, 13),
+    (10, 9),
+    (10, 6),
+    (10, 11),
+    (11, 7),
+    (11, 15),
+    (12, 8),
+    (13, 12),
+    (13, 9),
+    (14, 10),
+    (14, 12),
+    (15, 14),
+    (8, 9),
+    (9, 10),
+    (10, 6)
 ])
 
 
-# plot the graph
-plt.figure(figsize=(8,8))
-nx.draw(G, connectionstyle='arc3, rad = 0.1')
+plt.figure(figsize=(16,8)) # plotting a frame
+nx.draw(G, connectionstyle='arc3, rad = 0.1') # plotting the graph
 plt.show()  # pause before exiting
