4678ba4c91b545b0ac10b07af070b64717e161b2
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-11-27 15:29:31 +0300

    Update graph.py

--- Practical 04/GraphVisualization/graph.py
+++ Practical 04/GraphVisualization/graph.py
@@ -4,6 +4,29 @@ import networkx as nx
 
 # create a directed multi-graph
 G = nx.MultiDiGraph()
+rad = 0
+
+# G.add_nodes_from([
+#     (0, {"color": "red"}),
+#     (1, {"color": "red"}),
+#     (2, {"color": "red"}),
+#     (3, {"color": "red"}),
+#     (4, {"color": "red"}),
+#     (5, {"color": "red"}),
+#     (6, {"color": "red"}),
+#     (7, {"color": "green"}),
+#     (8, {"color": "green"}),
+#     (9, {"color": "green"}),
+#     (10, {"color": "red"}),
+#     (11, {"color": "red"}),
+#     (12, {"color": "red"}),
+#     (13, {"color": "red"}),
+#     (14, {"color": "red"}),
+#     (11, {"color": "red"}),
+#     (15, {"color": "red"}),
+# ])
+
+
 G.add_edges_from([
     (0, 1),
     (1, 5),
@@ -24,23 +47,23 @@ G.add_edges_from([
     (8, 9),
     (9, 10),
     (9, 13),
-    (10, 9),
     (10, 6),
+    (10, 9),
     (10, 11),
     (11, 7),
     (11, 15),
     (12, 8),
-    (13, 12),
     (13, 9),
+    (13, 12),
     (14, 10),
     (14, 12),
     (15, 14),
-    (8, 9),
-    (9, 10),
-    (10, 6)
 ])
 
 
 plt.figure(figsize=(16,8)) # plotting a frame
-nx.draw(G, connectionstyle='arc3, rad = 0.1') # plotting the graph
+nx.draw(G, connectionstyle=f'arc3, rad = {rad}', with_labels = True) # plotting the graph
 plt.show()  # pause before exiting
+
+
+G.clear()
